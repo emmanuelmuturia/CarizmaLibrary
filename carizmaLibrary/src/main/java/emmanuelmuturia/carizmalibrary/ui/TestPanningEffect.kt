efce1d897ca482c326c0a12f700dfe3439b4242d
Emmanuel Muturia <science@emmanuelmuturia.com>
2024-09-25 08:37:09 +0300

    Add the Jetpack Compose dependencies to :carizmaLibrary...

--- carizmaLibrary/src/main/java/emmanuelmuturia/carizmalibrary/ui/TestPanningEffect.kt
+++ carizmaLibrary/src/main/java/emmanuelmuturia/carizmalibrary/ui/TestPanningEffect.kt
@@ -1,6 +1,9 @@
 package emmanuelmuturia.carizmalibrary.ui
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
-fun TestPanningEffect(modifier: androidx.compose.ui.Modifier = androidx.compose.ui.Modifier) {
+@Composable
+fun TestPanningEffect(modifier: Modifier = Modifier) {
 
 }
