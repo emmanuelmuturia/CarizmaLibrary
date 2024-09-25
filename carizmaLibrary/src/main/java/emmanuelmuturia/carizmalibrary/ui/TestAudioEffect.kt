107440569282618e238f33aaaf01aee35db92345
Emmanuel Muturia <science@emmanuelmuturia.com>
2024-09-25 11:01:10 +0300

    Rename the TestStereoPanning file to TestAudioEffect...

--- carizmaLibrary/src/main/java/emmanuelmuturia/carizmalibrary/ui/TestAudioEffect.kt
+++ carizmaLibrary/src/main/java/emmanuelmuturia/carizmalibrary/ui/TestAudioEffect.kt
@@ -1,19 +1,14 @@
 package emmanuelmuturia.carizmalibrary.ui
 
 import android.media.MediaPlayer
-import android.net.Uri
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material.icons.rounded.Clear
 import androidx.compose.material.icons.rounded.PlayArrow
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -22,13 +17,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.media3.exoplayer.ExoPlayer
 import emmanuelmuturia.carizmalibrary.R
 import java.util.Timer
 import java.util.TimerTask
 
 @Composable
-fun TestStereoPanningEffect(modifier: Modifier = Modifier) {
+fun TestAudioEffect(modifier: Modifier = Modifier) {
 
     var isPlaying by remember { mutableStateOf(value = false) }
 
