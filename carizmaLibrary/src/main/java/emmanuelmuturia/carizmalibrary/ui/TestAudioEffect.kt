6f9fd53500aacc11b244b6a630472c5aca1f823e
Emmanuel Muturia <science@emmanuelmuturia.com>
2024-09-27 10:08:26 +0300

    Remove the Button...

--- carizmaLibrary/src/main/java/emmanuelmuturia/carizmalibrary/ui/TestAudioEffect.kt
+++ carizmaLibrary/src/main/java/emmanuelmuturia/carizmalibrary/ui/TestAudioEffect.kt
@@ -18,59 +18,13 @@ package emmanuelmuturia.carizmalibrary.ui
 
 import android.media.MediaPlayer
 import android.media.audiofx.EnvironmentalReverb
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Clear
-import androidx.compose.material.icons.rounded.PlayArrow
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import emmanuelmuturia.carizmalibrary.R
-import kotlin.math.sin
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
+import kotlin.math.sin
 
-@Composable
-fun TestAudioEffect(modifier: Modifier = Modifier) {
-    var isPlaying by remember { mutableStateOf(value = false) }
-
-    val context = LocalContext.current
-    var mediaPlayer = MediaPlayer.create(context, R.raw.solo)
-
-    Column(
-        modifier = modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        IconButton(onClick = {
-            isPlaying = !isPlaying
-            if (isPlaying) {
-                // applyAutoPanning(mediaPlayer = mediaPlayer)
-                // applyReverb(mediaPlayer = mediaPlayer)
-            } else {
-                mediaPlayer?.release()
-                mediaPlayer = null
-            }
-        }) {
-            Icon(
-                imageVector = if (isPlaying) Icons.Rounded.Clear else Icons.Rounded.PlayArrow,
-                contentDescription = ""
-            )
-        }
-    }
-}
 
 private fun applyAutoPanning(
     mediaPlayer: MediaPlayer,
