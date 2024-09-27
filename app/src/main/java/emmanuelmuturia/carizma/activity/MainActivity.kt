71641c2accfeaeff2bbd9e99bd52eef8a19a3a33
Emmanuel Muturia <science@emmanuelmuturia.com>
2024-09-27 10:25:36 +0300

    Remove unnecessary components...

--- app/src/main/java/emmanuelmuturia/carizma/activity/MainActivity.kt
+++ app/src/main/java/emmanuelmuturia/carizma/activity/MainActivity.kt
@@ -20,12 +20,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
 import emmanuelmuturia.carizma.theme.CarizmaLibraryTheme
-import emmanuelmuturia.carizmalibrary.ui.TestAudioEffect
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,9 +28,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CarizmaLibraryTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { paddingValues ->
-                    TestAudioEffect(modifier = Modifier.padding(paddingValues = paddingValues))
-                }
+
             }
         }
     }
