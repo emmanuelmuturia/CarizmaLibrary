bc44d557dd9a9465a6e5aa86de24d21c50932c62
Emmanuel Muturia <science@emmanuelmuturia.com>
2024-09-30 09:42:29 +0300

    Re-introduce the TestAudioEffects Composable for Testing purposes...

--- app/src/main/java/emmanuelmuturia/carizma/activity/MainActivity.kt
+++ app/src/main/java/emmanuelmuturia/carizma/activity/MainActivity.kt
@@ -16,11 +16,34 @@
  */
 package emmanuelmuturia.carizma.activity
 
+import android.content.Context
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Clear
+import androidx.compose.material.icons.rounded.PlayArrow
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import emmanuelmuturia.carizmalibrary.R
 import emmanuelmuturia.carizma.theme.CarizmaLibraryTheme
+import emmanuelmuturia.carizmalibrary.data.extensions.to8D
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,7 +51,47 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CarizmaLibraryTheme {
+                Scaffold { paddingValues ->
+                    TestAudioEffects(
+                        modifier = Modifier.padding(paddingValues = paddingValues),
+                        context = this
+                    )
+                }
             }
+        }
+    }
+}
+
+@Composable
+fun TestAudioEffects(modifier: Modifier = Modifier, context: Context) {
+    var isPlaying by remember { mutableStateOf(value = false) }
+
+    var mediaPlayer = MediaPlayer.create(context, R.raw.solo)
+
+    val coroutineScope = rememberCoroutineScope()
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        IconButton(onClick = {
+            isPlaying = !isPlaying
+            if (isPlaying) {
+                coroutineScope.launch {
+                    mediaPlayer.to8D(
+                        mediaPlayer = mediaPlayer
+                    )
+                }
+            } else {
+                mediaPlayer?.release()
+                mediaPlayer = null
+            }
+        }) {
+            Icon(
+                imageVector = if (isPlaying) Icons.Rounded.Clear else Icons.Rounded.PlayArrow,
+                contentDescription = ""
+            )
         }
     }
 }
