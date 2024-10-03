4327f7bf888677e527b3a32bb83607b6d81a6d22
Emmanuel Muturia <science@emmanuelmuturia.com>
2024-10-03 10:12:52 +0300

    Rename the Project and Copyright...

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,23 +1,6 @@
-/*
- * Copyright 2024 Carizma Library
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
 plugins {
-    alias(notation = libs.plugins.android.application)
-    alias(notation = libs.plugins.kotlin.android)
-    alias(notation = libs.plugins.compose.compiler)
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
 }
 
 android {
@@ -25,66 +8,36 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "emmanuelmuturia.sonux"
         minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
             proguardFiles(
-                files = arrayOf(
-                    getDefaultProguardFile(name = "proguard-android-optimize.txt"),
-                    "proguard-rules.pro"
-                )
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
             )
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
-        jvmTarget = "17"
-    }
-    buildFeatures {
-        compose = true
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
+        jvmTarget = "1.8"
     }
 }
 
 dependencies {
 
-    implementation(dependencyNotation = project(path = ":carizmaLibrary"))
-
-    implementation(dependencyNotation = libs.androidx.core.ktx)
-    implementation(dependencyNotation = libs.androidx.lifecycle.runtime.ktx)
-    implementation(dependencyNotation = libs.androidx.activity.compose)
-    implementation(dependencyNotation = platform(libs.androidx.compose.bom))
-    implementation(dependencyNotation = libs.androidx.ui)
-    implementation(dependencyNotation = libs.androidx.ui.graphics)
-    implementation(dependencyNotation = libs.androidx.ui.tooling.preview)
-    implementation(dependencyNotation = libs.androidx.material3)
-
-    testImplementation(dependencyNotation = libs.junit)
-
-    androidTestImplementation(dependencyNotation = libs.androidx.junit)
-    androidTestImplementation(dependencyNotation = libs.androidx.espresso.core)
-    androidTestImplementation(dependencyNotation = platform(libs.androidx.compose.bom))
-    androidTestImplementation(dependencyNotation = libs.androidx.ui.test.junit4)
-
-    debugImplementation(dependencyNotation = libs.androidx.ui.tooling)
-    debugImplementation(dependencyNotation = libs.androidx.ui.test.manifest)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
 }
