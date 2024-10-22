921b08c49d44529ba240889026f9ca6be3316ca9
Emmanuel Muturia <science@emmanuelmuturia.com>
2024-10-22 16:57:07 +0300

    Set up Navigator...

--- composeApp/src/commonMain/kotlin/emmanuelmuturia/sonux/app/App.kt
+++ composeApp/src/commonMain/kotlin/emmanuelmuturia/sonux/app/App.kt
@@ -22,45 +22,14 @@
 */
 package emmanuelmuturia.sonux.app
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import emmanuelmuturia.sonux.samples.Greeting
-import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
-import sonux.composeapp.generated.resources.Res
-import sonux.composeapp.generated.resources.compose_multiplatform
+import cafe.adriel.voyager.navigator.Navigator
+import emmanuelmuturia.sonux.screens.HomeScreen
+import emmanuelmuturia.sonux.ui.theme.SonuxSharedTheme
 
 @Composable
-@Preview
 fun App() {
-    MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
-        }
+    SonuxSharedTheme {
+        Navigator(screen = HomeScreen())
     }
 }
