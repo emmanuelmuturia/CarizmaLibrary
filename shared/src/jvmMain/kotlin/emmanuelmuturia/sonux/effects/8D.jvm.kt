8219088e6e49528c91bdf178ccdab891d4586de1
Emmanuel Muturia <science@emmanuelmuturia.com>
2024-10-23 12:31:25 +0300

    Format the code...

--- shared/src/jvmMain/kotlin/emmanuelmuturia/sonux/effects/8D.jvm.kt
+++ shared/src/jvmMain/kotlin/emmanuelmuturia/sonux/effects/8D.jvm.kt
@@ -1,2 +1,10 @@
 package emmanuelmuturia.sonux.effects
 
+import kotlinx.coroutines.CoroutineDispatcher
+
+internal actual suspend fun convertAudioTo8D(
+    frequency: Float,
+    amount: Float,
+    coroutineDispatcher: CoroutineDispatcher
+) {
+}
