5cddcced68845f52903e4d81b8469e570ec5f658
Emmanuel Muturia <science@emmanuelmuturia.com>
2024-10-24 11:00:59 +0300

    Implement the Permission Request Functionality (Android Runtime Permissions)...

--- shared/src/androidMain/kotlin/emmanuelmuturia/sonux/permissions/Permissions.kt
+++ shared/src/androidMain/kotlin/emmanuelmuturia/sonux/permissions/Permissions.kt
@@ -1,2 +1,26 @@
 package emmanuelmuturia.sonux.permissions
 
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+fun checkIfPermissionIsGranted(
+    context: Context,
+    permission: String
+): Boolean {
+    return (ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED)
+}
+
+fun shouldShowPermissionRationale(
+    context: Context,
+    permission: String
+): Boolean {
+    val activity = context as Activity?
+    if (activity == null)
+        Log.d("Permissions", "Activity is null")
+
+    return ActivityCompat.shouldShowRequestPermissionRationale(activity!!, permission)
+}
