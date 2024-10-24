e8601559e22eb8ce4f1f8f67a74a4a7cd0b8ba6b
Emmanuel Muturia <science@emmanuelmuturia.com>
2024-10-24 20:36:59 +0300

    Remove the useless FFMPEG dependency...

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -26,7 +26,6 @@ kotlin {
             implementation(dependencyNotation = libs.kotlinx.coroutines.android)
             implementation(dependencyNotation = libs.androidx.core)
             implementation(dependencyNotation = libs.bundles.koin)
-            implementation(dependencyNotation = "com.arthenica:ffmpeg-kit-full:4.5")
         }
     }
 }
